28f8a3f066ce1abb54742336c22f31ef3d9f51fb
Ayaan Dey <ayaan@itobuz.com>
2024-12-26 18:41:52 +0530

    first-commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function aqua() {
+    document.body.style.backgroundColor = "aqua";
+}
+
+function blue() {
+    document.body.style.backgroundColor = "blue";
+}
+
+function grey() {
+    document.body.style.backgroundColor = "grey";
+}
+
+function green() {
+    document.body.style.backgroundColor = "green";
+}
